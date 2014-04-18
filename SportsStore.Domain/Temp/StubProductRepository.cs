3905c79a80154858d8efd20864da34c4f95e432a
Ferenc Grecu <mivor20@gmail.com>
2014-04-18 14:14:52 +0300

    Initial commit

---
+++ SportsStore.Domain/Temp/StubProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Enitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Temp
+{
+    public class StubProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products { get; private set; }
+        
+        public StubProductRepository()
+        {
+            Products = new List<Product> 
+            {
+                new Product { Name = "Football", Price = 25},
+                new Product { Name = "Surf Board", Price = 179},
+                new Product { Name = "Running Shoes", Price = 95}
+            }.AsQueryable();
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Enitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _repository = productRepository;
+        }
+
+        public ActionResult List()
+        {
+            return View(_repository.Products);
+        }
+
+    }
+}
